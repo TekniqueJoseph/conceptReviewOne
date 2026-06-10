745e9c3cc3c2af2342e6080409283343657720aa
TekniqueJoseph <joejacksun@gmail.com>
2026-06-10 07:14:57 -0400

    styling

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 document.querySelectorAll('.headerCenter').forEach(item => {
     item.style.textAlign = 'center'
 }) 
+  // Get the current date and format it locally
+  const today = new Date().toLocaleDateString();
+  
+  // Inject it into the HTML span
+  document.getElementById('current-date').textContent = today;
  
