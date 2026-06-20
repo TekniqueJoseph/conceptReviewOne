d7247f1beb93f62317ffa1957e6582c23b0bd388
TekniqueJoseph <joejacksun@gmail.com>
2026-06-19 22:38:01 -0400

    positioned footer

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-document.querySelector('.headerCenter').style.textAlign = 'center'
 // document.querySelectorAll('.headerCenter').forEach(item => {
 //     item.style.textAlign = 'center'
 // }) 
+// document.querySelector('.journey').style.textTransform = 'capitalize';
 
-  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const todayIndex = new Date().getDay(); // Returns a number 0-6
-  document.getElementById("current-day").textContent = days[todayIndex];
+let cent = document.querySelector('.headerCenter');
+if(cent){
+    cent.style.textAlign = 'center'
+}
 
-  // Get the current date and format it locally
-  const today = new Date().toLocaleDateString();
-  // Inject it into the HTML span
-  document.getElementById('current-date').textContent = today;
- 
+let cap = document.querySelector('.journey');
+if(cap){
+cap.style.textTransform = 'capitalize'
+}
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const todayIndex = new Date().getDay(); // Returns a number 0-6
+// document.getElementById("current-day").textContent = days[todayIndex];
+let cday = document.getElementById("current-day");
+if(cday) {
+    cday.textContent = days[todayIndex];
+}
+
+// Get the current date and format it locally
+const today = new Date().toLocaleDateString();
+// Inject it into the HTML span
+// document.getElementById('current-date').textContent = today;
+let cdate = document.getElementById('current-date');
+if(cdate){
+    cdate.textContent = today
+}
+
+
